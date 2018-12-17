7c91cbbad8d87b62ff2341bbec33e61942c05048
guilhermo <guilhermo.readonly@gmail.com>
2018-12-17 21:24:38 +0100

    handle error better + add logging

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,61 @@
-extern crate sdl2;
+#[macro_use]
+extern crate log;
 
+use std::process;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
+
 
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
+    let sdl_context = match sdl2::init() {
+        Ok(f) => f,
+        Err(error) => {
+            error!("An error occured while getting the sdl context: {:?}", error);
+            process::exit(1);
+        },
+    };
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let video_subsystem = match sdl_context.video() {
+        Ok(f) => f,
+        Err(error) => {
+            error!("An error occured while getting the video subsystem: {:?}", error);
+            process::exit(1);
+        },
+    };
+
+    let window = match video_subsystem
+    .window("asteroids", 800, 600)
+    .position_centered()
+    .build() {
+        Ok(f) => f,
+        Err(error) => {
+            error!("An error occured while building the window: {:?}", error);
+            process::exit(1);
+        },
+    };
+
+    let mut canvas = match window.into_canvas().build() {
+        Ok(f) => f,
+        Err(error) => {
+            error!("An error occured while building the canvas: {:?}", error);
+            process::exit(1);
+        },
+    };
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = match sdl_context.event_pump() {
+        Ok(f) => f,
+        Err(error) => {
+            error!("An error occured while getting the event pump: {:?}", error);
+            process::exit(1);
+        },
+    };
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -34,9 +65,12 @@ pub fn main() {
                 _ => {}
             }
         }
+
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
         // The rest of the game loop goes here...
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
